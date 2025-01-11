b02c5bc4792f3099eac98ab3d740cb2b16a9d977
GkingOficial <talisoncaedoso@gmail.com>
2025-01-11 02:51:43 -0300

    Configurando variáveis de ambiente e ignores

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u@ra%wgc(wiyel8vdl8%#7k!=corwd60-dtvej1&+&(mum)%ql'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
 
