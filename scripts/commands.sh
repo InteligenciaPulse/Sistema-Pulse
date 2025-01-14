55cce04ac00e3c2db902200e4d78c548d7b3b834
GkingOficial <talisoncaedoso@gmail.com>
2025-01-14 17:46:29 -0300

    commands.sh convertido de DOS para UNIX

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -5,7 +5,7 @@ set -e
 
 while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
   echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
-  sleep 2
+  sleep 3
 done
 
 echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
