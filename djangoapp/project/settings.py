7ef8062c7726b620293ee70718a1e5e5ffdb69bc
GkingOficial <talisoncaedoso@gmail.com>
2025-01-30 19:14:52 -0300

    Configurado corretamente arquivos estáticos

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -126,10 +126,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'cadastro/static'),
+    DATA_DIR / 'static',
+]
+
 STATIC_URL = '/static/'
-STATIC_ROOT = DATA_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = DATA_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
