b4c4cbc7b53a7eb099cd3bc3fe34edadd509acb5
GkingOficial <talisoncaedoso@gmail.com>
2025-02-13 20:09:13 -0300

    Modificado configurações do settings.py e do Dockerfile para tentar fazer com que o Railway sirva arquivos estáticos

--- Dockerfile
+++ Dockerfile
@@ -46,5 +46,7 @@ ENV PATH="/scripts:/venv/bin:$PATH"
 # Muda o usuário para duser
 USER duser
 
+RUN python manage.py collectstatic --noinput
+
 # Executa o arquivo scripts/commands.sh
 CMD ["commands.sh"]
--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,13 +127,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'cadastro/static'),
     # DATA_DIR / 'static',
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+# STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = DATA_DIR / 'media'
