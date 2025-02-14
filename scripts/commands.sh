e4ea8f90b67e34df72c76c484f45f8100ebe9d20
GkingOficial <talisoncaedoso@gmail.com>
2025-02-14 11:42:08 -0300

    Adicionado ls no Dockerfile para debugging

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -11,9 +11,6 @@ done
 echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
 
 python manage.py collectstatic --noinput
-
-ls -lah /data/web/static/cadastro/css && ls -lah /data/web/static/cadastro/js
-
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py runserver 0.0.0.0:8000
